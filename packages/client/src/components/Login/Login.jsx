0988bfd1a0229bf4be4499281bca22d33ef2d47d
Emre Yoleri <emreyoleri01@gmail.com>
2023-02-02 04:17:13 +0300

    Sharing Validation Code Between React and NodeJS

---
+++ packages/client/src/components/Login/Login.jsx
@@ -0,0 +1,73 @@
+import { Button, ButtonGroup, Heading, VStack } from "@chakra-ui/react";
+import { Form, Formik } from "formik";
+import { useNavigate } from "react-router";
+import TextField from "./TextField";
+import { formSchema } from "@whatsapp-clone/common";
+
+const Login = () => {
+  const navigate = useNavigate();
+  return (
+    <Formik
+      initialValues={{ username: "", password: "" }}
+      validationSchema={formSchema}
+      onSubmit={(values, actions) => {
+        const vals = { ...values };
+        actions.resetForm();
+        fetch("http://localhost:4000/auth/login", {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(vals),
+        })
+          .catch((err) => {
+            return;
+          })
+          .then((res) => {
+            if (!res || !res.ok || res.status >= 400) {
+              return;
+            }
+            return res.json();
+          })
+          .then((data) => {
+            if (!data) return;
+            console.log(data);
+          });
+      }}
+    >
+      <VStack
+        as={Form}
+        w={{ base: "90%", md: "500px" }}
+        m="auto"
+        justify="center"
+        h="100vh"
+        spacing="1rem"
+      >
+        <Heading>Log In</Heading>
+        <TextField
+          name="username"
+          placeholder="Enter username"
+          autoComplete="off"
+          label="Username"
+        />
+
+        <TextField
+          name="password"
+          placeholder="Enter password"
+          autoComplete="off"
+          label="Password"
+        />
+
+        <ButtonGroup pt="1rem">
+          <Button colorScheme="teal" type="submit">
+            Log In
+          </Button>
+          <Button onClick={() => navigate("/register")}>Create Account</Button>
+        </ButtonGroup>
+      </VStack>
+    </Formik>
+  );
+};
+
+export default Login;
---
+++ packages/client/src/components/Login/SignUp.jsx
@@ -0,0 +1,76 @@
+import { ArrowBackIcon } from "@chakra-ui/icons";
+import { Button, ButtonGroup, Heading, VStack } from "@chakra-ui/react";
+import { Form, Formik } from "formik";
+import { useNavigate } from "react-router";
+import { formSchema } from "@whatsapp-clone/common";
+import TextField from "./TextField";
+
+const SignUp = () => {
+  const navigate = useNavigate();
+  return (
+    <Formik
+      initialValues={{ username: "", password: "" }}
+      validationSchema={formSchema}
+      onSubmit={(values, actions) => {
+        const vals = { ...values };
+        actions.resetForm();
+        fetch("http://localhost:4000/auth/register", {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(vals),
+        })
+          .catch((err) => {
+            return;
+          })
+          .then((res) => {
+            if (!res || !res.ok || res.status >= 400) {
+              return;
+            }
+            return res.json();
+          })
+          .then((data) => {
+            if (!data) return;
+            console.log(data);
+          });
+      }}
+    >
+      <VStack
+        as={Form}
+        w={{ base: "90%", md: "500px" }}
+        m="auto"
+        justify="center"
+        h="100vh"
+        spacing="1rem"
+      >
+        <Heading>Sign Up</Heading>
+        <TextField
+          name="username"
+          placeholder="Enter username"
+          autoComplete="off"
+          label="Username"
+        />
+
+        <TextField
+          name="password"
+          placeholder="Enter password"
+          autoComplete="off"
+          label="Password"
+        />
+
+        <ButtonGroup pt="1rem">
+          <Button colorScheme="teal" type="submit">
+            Create Account
+          </Button>
+          <Button onClick={() => navigate("/")} leftIcon={<ArrowBackIcon />}>
+            Back
+          </Button>
+        </ButtonGroup>
+      </VStack>
+    </Formik>
+  );
+};
+
+export default SignUp;
